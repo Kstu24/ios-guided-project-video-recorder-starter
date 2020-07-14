98c1007abd281b059dbeaee1800eb99774f088e7
Kevin Stewart <kistewart419@gmail.com>
2020-07-13 22:01:10 -0400

    Added Audio authorization switch statement and microphone + camera usage in plist

--- VideoRecorder/VideoRecorder/ViewController.swift
+++ VideoRecorder/VideoRecorder/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 	
@@ -20,9 +21,36 @@ class ViewController: UIViewController {
 		
 		// TODO: get permission
 		
-		showCamera()
+		requestVideoPermission()
 		
 	}
+    
+    private func requestPermissionAndShowCamera() {
+        switch AVCaptureDevice.authorizationStatus(for: .video) {
+        case .notDetermined:
+            requestVideoPermission()
+        case .restricted:
+            preconditionFailure("Video is disabled, please review device restrictions.")
+        case .denied:
+            preconditionFailure("DENIED! Tell the user they can't use the app without giving permissions via Settings > Privacy > Video")
+        case .authorized:
+            showCamera()
+            
+        @unknown default:
+            preconditionFailure("A new status code was added that we need to handle.")
+        }
+    }
+    
+    private func requestVideoPermission() {
+        AVCaptureDevice.requestAccess(for: .video) { (isGranted) in
+            guard isGranted else {
+                preconditionFailure("UI: Tell the user to enable permissions for Video/Camera.")
+            }
+                DispatchQueue.main.async {
+                    self.showCamera()
+                }
+            }
+        }
 	
 	private func showCamera() {
 		performSegue(withIdentifier: "ShowCamera", sender: self)
