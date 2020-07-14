3dd713e9716889ddf9f4512593b77709d1184510
Kevin Stewart <kistewart419@gmail.com>
2020-07-13 22:17:59 -0400

    Set up camera

--- VideoRecorder/VideoRecorder/CameraViewController.swift
+++ VideoRecorder/VideoRecorder/CameraViewController.swift
@@ -7,8 +7,11 @@
 //
 
 import UIKit
+import AVFoundation
 
 class CameraViewController: UIViewController {
+    
+    lazy private var captureSession = AVCaptureSession()
 
     @IBOutlet var recordButton: UIButton!
     @IBOutlet var cameraView: CameraPreviewView!
@@ -19,8 +22,39 @@ class CameraViewController: UIViewController {
 
 		// Resize camera preview to fill the entire screen
 		cameraView.videoPreviewLayer.videoGravity = .resizeAspectFill
+        setUpCamera()
 	}
-
+    
+    private func setUpCamera() {
+        let camera = bestCamera()
+        
+        captureSession.beginConfiguration() // starts configuration
+        guard let cameraInput = try? AVCaptureDeviceInput(device: camera) else {
+            preconditionFailure("Can't create an input from the camera.")
+        }
+        
+        guard captureSession.canAddInput(cameraInput) else { // Returns a Boolean value that indicates whether a given input can be added to the session.
+            preconditionFailure("This session can't handle this trype of input: \(cameraInput)")
+        }
+        
+        captureSession.addInput(cameraInput) // Adds a given input to the session.
+        
+        captureSession.commitConfiguration() // ends configuration
+    }
+    
+    private func bestCamera() -> AVCaptureDevice {
+        if let device = AVCaptureDevice.default(.builtInUltraWideCamera,
+                                                for: .video,
+                                                position: .back) {
+            return device
+        }
+        if let device = AVCaptureDevice.default(.builtInWideAngleCamera,
+                                                for: .video,
+                                                position: .back) {
+            return device
+        }
+        preconditionFailure("No cameras on device match the specs that we need.")
+    }
 
     @IBAction func recordButtonPressed(_ sender: Any) {
 
